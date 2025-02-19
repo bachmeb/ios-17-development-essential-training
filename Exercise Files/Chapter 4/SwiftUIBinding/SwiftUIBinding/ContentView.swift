dd3b05070f8591078f82869f922f49a8ac4211eb
bachmeb <bachmeb@mail.uc.edu>
2025-02-18 21:58:16 -0500

    add color slider

--- Exercise Files/Chapter 4/SwiftUIBinding/SwiftUIBinding/ContentView.swift
+++ Exercise Files/Chapter 4/SwiftUIBinding/SwiftUIBinding/ContentView.swift
@@ -8,7 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     @State   var text = "Hello, world!"
+    @State var red: Double = 1.0
     var body: some View {
         VStack {
             TextField("Type your name", text: $text)
@@ -16,6 +18,14 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text(text)
+            Text("Color Picker")
+                .font(.title)
+                .fontWeight(.bold)
+            Image(systemName: "dog.circle")
+                .resizable()
+                .frame(width: 100, height: 100)
+                .foregroundColor(Color(red: red, green: 0.0, blue: 0.0))
+            Slider(value: $red)
         }
         .padding()
     }
