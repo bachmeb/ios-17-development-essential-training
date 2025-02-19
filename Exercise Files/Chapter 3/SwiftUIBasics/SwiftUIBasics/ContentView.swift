425d2c6d1639ad97f9a32c4e7414251c1c6c8b8a
bachmeb <bachmeb@mail.uc.edu>
2025-02-18 21:13:23 -0500

    add days of the week

--- Exercise Files/Chapter 3/SwiftUIBasics/SwiftUIBasics/ContentView.swift
+++ Exercise Files/Chapter 3/SwiftUIBasics/SwiftUIBasics/ContentView.swift
@@ -21,6 +21,48 @@ struct ContentView: View {
                 Text("Sunday")
 
             }
+            HStack {
+                Image(systemName: "moon.fill")
+                    .imageScale(.large)
+                    .foregroundStyle(.gray)
+                Text("Monday")
+
+            }
+            HStack {
+                Image(systemName: "t.circle.fill")
+                    .imageScale(.large)
+                    .foregroundStyle(.indigo)
+                Text("Tuesday")
+
+            }
+            HStack {
+                Image(systemName: "w.circle.fill")
+                    .imageScale(.large)
+                    .foregroundStyle(.blue)
+                Text("Wednesday")
+
+            }
+            HStack {
+                Image(systemName: "t.circle.fill")
+                    .imageScale(.large)
+                    .foregroundStyle(.green)
+                Text("Thursday")
+
+            }
+            HStack {
+                Image(systemName: "f.circle.fill")
+                    .imageScale(.large)
+                    .foregroundStyle(.red)
+                Text("Friday")
+
+            }
+            HStack {
+                Image(systemName: "s.circle.fill")
+                    .imageScale(.large)
+                    .foregroundStyle(.orange)
+                Text("Saturday")
+
+            }
             Image("4571473")
                 .resizable()
                 .scaledToFit()
